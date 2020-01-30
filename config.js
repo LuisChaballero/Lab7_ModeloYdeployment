ca557a4d3ee759fd7fc2c69e20fa34e8581819ea
Luis Caballero <luis_cn98@hotmail.com>
2020-01-29 21:32:55 -0600

    Added script start for deployment

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
-
-exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost/blog";
+// mongodb+srv://luis:luis@cluster0-g8ctf.mongodb.net/blog?retryWrites=true&w=majority
+// mongodb://localhost/blog
+exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb+srv://luis:luis@cluster0-g8ctf.mongodb.net/blog?retryWrites=true&w=majority";
 exports.PORT = process.env.PORT || 8080;
